15c64dee68192a3dcf080c41e2f53f5453e964a5
Luke Chen <luke.chen.2010@gmail.com>
2020-06-21 15:34:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_doctors.rb
+++ db/migrate/001_create_doctors.rb
@@ -1,9 +1,8 @@
 class CreateDoctors < ActiveRecord::Migration
   def change
     create_table :doctors do |t|
-      t.string :content
-      t.belongs_to :user
-      t.belongs_to :post
+      t.string :name
+      t.string :department
       t.timestamps null: false
     end
   end
