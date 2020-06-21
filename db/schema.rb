a3a1e9fe2591fa1c98bc4f73d7fc788f39c78002
Luke Chen <luke.chen.2010@gmail.com>
2020-06-21 15:40:49 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
-    t.integer  "user_id"
-    t.integer  "post_id"
+    t.integer  "doctor_id"
+    t.integer  "patient_id"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
