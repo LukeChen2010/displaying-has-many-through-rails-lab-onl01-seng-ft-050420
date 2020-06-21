21fb8dc1157d81889c1e13eb805ffceeb324a993
Luke Chen <luke.chen.2010@gmail.com>
2020-06-21 15:35:29 +0000

    Automatically backed up by Learn

---
+++ db/migrate/003_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration
+  def change
+    create_table :appointments do |t|
+      t.string :content
+      t.belongs_to :user
+      t.belongs_to :post
+      t.timestamps null: false
+    end
+  end
+end
